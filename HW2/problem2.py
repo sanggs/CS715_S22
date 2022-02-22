2a5e84b54acce6651e856c8730f0de52d607d3e4
sanggs <sangeethagramas@gmail.com>
2022-02-21 20:36:32 -0600

    HW2 files

---
+++ HW2/problem2.py
@@ -0,0 +1,107 @@
+from cProfile import label
+import numpy as np
+import matplotlib.pyplot as plt
+from torch import norm, threshold
+
+def generate_random_matrix(m):
+    mat = np.random.randn(m, m)
+    return mat
+
+def make_upper_traingular(mat):
+    return np.triu(mat)
+
+def normalize_matrix(mat, m):
+    mat = mat / np.sqrt(m)
+    return mat
+
+def get_eigen_values(mat):
+    v, _ = np.linalg.eig(mat)
+    return v
+
+def get_spectral_radius(eig_vals):
+    return np.max(np.absolute(eig_vals))
+
+def get_norm_2(mat):
+    return np.linalg.norm(mat, 2)
+
+def get_smallest_sigma(mat):
+    sg_min = np.min(np.linalg.svd(mat, compute_uv=False))
+    return sg_min
+
+def plot_eig_vals(eig_values, m_sizes, markers):
+    plt.clf()
+    plt.cla()
+    fig, (ax1, ax2) = plt.subplots(1, 2)
+    num_plots = len(m_sizes)
+    for i in range(num_plots):
+        ei = eig_values[i]
+        ei_real = np.real(ei)
+        ei_complex = np.imag(ei)
+        m = m_sizes[i]
+        y = np.arange(m)
+        ax1.plot(ei_real, y, markers[i], label='Matrix dim = '+str(m))
+        ax2.plot(ei_complex, y, markers[i], label='Matrix dim = '+str(m))
+    ax1.legend()
+    ax1.set_title('Real part of eigen values')
+    ax2.legend()
+    ax2.set_title('Imaginary part of eigen values')
+    # plt.legend()
+    plt.show()
+
+def plot_spectral_radius(sr, nm, m):
+    plt.clf()
+    plt.cla()
+    plt.plot(m, sr, label='Spectral radius')
+    plt.plot(m, nm, label='2-Norm')
+    plt.legend()
+    plt.show()
+
+def plot_min_singular_distribution(m_list, threshold):
+    plt.clf()
+    plt.cla()
+    fig, (ax1, ax2) = plt.subplots(1, 2)
+    sg_min_tail = []
+    for m in m_list:
+        sg_min_list = []
+        count = 0
+        for i in range(80):
+            mat = generate_random_matrix(m)
+            sg_min = get_smallest_sigma(mat)
+            sg_min_list.append(sg_min)
+            if (sg_min < threshold):
+                count += 1
+        sg_min_tail.append(count)
+        ax1.plot(sg_min_list, 'o', label='Matrix dim = {}'.format(m))
+    ax2.plot(m_list, sg_min_tail, 'o-')
+    ax1.set_title('Singular value distribution')
+    ax1.legend()
+    ax2.set_title('Number of singular values less than {}'.format(threshold))
+    plt.show()
+    return
+
+def run_experiment(m_list):
+    eig_values = []
+    spectral_radius = []
+    norm_2 = []
+    threshold = 1./np.power(2, 3)
+    for m in m_list:
+        mat = generate_random_matrix(m)
+        # mat = make_upper_traingular(mat)
+        mat = normalize_matrix(mat, m)
+        ei = get_eigen_values(mat)
+        eig_values.append(ei)
+        sr = get_spectral_radius(ei)
+        spectral_radius.append(sr)
+        n2 = get_norm_2(mat)
+        norm_2.append(n2)
+    markers = ['>', 'o', '*', 's', "p", "+", "v", "^", "x"]
+    plot_eig_vals(eig_values, m_list, markers)
+    plot_spectral_radius(spectral_radius, norm_2, m_list)
+    # m_copy = [m_list[4]]
+    plot_min_singular_distribution(m_list, threshold)
+
+if __name__ == '__main__':
+    m_list = [8, 16, 32, 64, 128, 256, 512, 1024]
+    run_experiment(m_list)
+
+
---
+++ HW2/problem4.py
@@ -0,0 +1,201 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy as scp
+from scipy import linalg as scplinalg
+from torch import permute
+import time
+
+def construct_1D_laplace(m):
+    diagonal = -2. * np.ones(m)
+    off_diag = np.ones(m-1)
+    return np.diag(diagonal, 0) + np.diag(off_diag, 1) + np.diag(off_diag, -1)
+
+def construct_rhs(m):
+    b = np.arange(m)
+    b = b.astype(np.float64)
+    return b
+
+def get_spectral_radius(mat):
+    ei = np.linalg.eig(mat)
+    sr = np.max(np.absolute(ei))
+    return sr
+
+def get_singular_value_min_max(mat):
+    sg = np.linalg.svd(mat, compute_uv=False)
+    return np.max(sg), np.min(sg)
+
+def get_frobenius_norm(mat):
+    return np.linalg.norm(mat, 'fro')
+
+def get_2_norm(mat):
+    return np.linalg.norm(mat, 2)
+
+def compute_condition_number(mat):
+    return np.linalg.cond(mat)
+
+# Get Spectral radius, condition number, sigma_max/min, matrix norm fro, 2
+def get_matrix_characteristics(mat):
+    sr = get_spectral_radius(mat)
+    sg_max, sg_min = get_singular_value_min_max(mat)
+    norm_f = get_frobenius_norm(mat)
+    norm_2 = get_frobenius_norm(mat)
+    cnum = compute_condition_number(mat)
+    return sr, sg_max, sg_min, cnum, norm_f, norm_2
+
+# QR Decomposition
+def get_QR_decomposition(mat):
+    q, r = np.linalg.qr(mat)
+    return q, r
+
+def check_qr_correctness(mat, q, r):
+    diff = np.linalg.norm(mat - np.matmul(q, r))
+    print("Diff in QR = {}".format(diff))
+
+def solve_using_qr(q, r, b):
+    start = time.time()
+    y = np.matmul(np.transpose(q), b)
+    end = time.time() - start
+    x = scplinalg.solve_triangular(r, y)
+    return x, end
+
+# LU Decomposition
+def get_lu_decomposition(mat):
+    p, l, u = scp.linalg.lu(mat, permute_l=False)
+    return p, l, u
+
+def check_lu_correctness(mat, p, l, u):
+    diff = np.linalg.norm(mat - p @ l @ u)
+    print("Diff in LU = {}".format(diff))
+
+def solve_using_lu(p, l, u, b):
+    b1 = np.matmul(np.transpose(p), b)
+    y = scplinalg.solve_triangular(l, b1, lower=True)
+    x = scplinalg.solve_triangular(u, y, lower=False)
+    return x 
+
+# SVD Decomposition
+def get_svd_decomposition(mat):
+    u, sigma, vh = np.linalg.svd(mat)
+    return u, sigma, vh
+
+def check_svd_correctness(mat, u, sigma, vh):
+    diff = np.linalg.norm(mat - u @ np.diag(sigma) @ vh)
+    print("Diff in SVD = {}".format(diff))
+
+def solve_using_svd(u, sigma, vh, b):
+    c = np.dot(np.transpose(u),b)
+    w = np.dot(np.diag(1./sigma),c)
+    # Vh x = w <=> x = Vh.H w (where .H stands for hermitian = conjugate transpose)
+    x = np.dot(np.transpose(vh),w)
+    return x
+
+# Cholesky Decomposition
+def get_cholesky_decomposition(mat):
+    L = np.linalg.cholesky(mat)
+    return L
+
+def check_cholesky_correctness(mat, L):
+    diff = np.linalg.norm(mat - np.matmul(L, np.transpose(L)))
+    print("Diff in Cholesky = {}".format(diff))
+
+def solve_using_cholesky(L, b):
+    y = scplinalg.solve_triangular(L, b, lower=True)
+    x = scplinalg.solve_triangular(np.transpose(L), y, lower=False)
+    return x
+
+# Solve using numpy.linalg.solve
+def solve_using_np_solve(mat, b):
+    x = np.linalg.solve(mat, b)
+    return x
+
+# Verify norm of |b-Ax|
+def verify_solution(mat, x, b):
+    diff = np.linalg.norm(np.matmul(mat, x) - b)
+    print("|b - Ax| = {}".format(diff))
+
+# 
+def log_plot_qr_time():
+    times = []
+    m_list = []
+    for m in range(3, 1001):
+        mat = construct_1D_laplace(m)
+        b = construct_rhs(m)
+        time_qr = time.time()
+        q, r = get_QR_decomposition(mat)
+        y = np.matmul(np.transpose(q), b)
+        time_qr = time.time() - time_qr
+        y = y + 1
+        times.append(np.log10(time_qr))
+        m_list.append(np.log10(m))
+    plt.clf()
+    plt.cla()
+    plt.plot(m_list, times)
+    plt.show()
+
+def run_experiment(m):
+    mat = construct_1D_laplace(m)
+    b = construct_rhs(m)
+
+    # Log time vs Log dimension
+    log_plot_qr_time()
+
+    exit(0)
+
+    # Solve using QR
+    print("Solve using QR")
+    time_qr = time.time()
+    q, r = get_QR_decomposition(mat)
+    time_qr = time.time() - time_qr
+    check_qr_correctness(mat, q, r)
+    x_qr, time_qr1 = solve_using_qr(q, r, b)
+    time_qr += time_qr1
+    print("Time taken for QR decomposition and y=Q*b = {} s".format(time_qr))
+    verify_solution(mat, x_qr, b)
+
+    # Solve using LU
+    print("Solve using LU")
+    time_lu = time.time()
+    p, l, u = get_lu_decomposition(mat)
+    time_lu = time.time() - time_lu
+    check_lu_correctness(mat, p, l, u)
+    time_lu_solve = time.time()
+    x_lu = solve_using_lu(p, l, u, b)
+    time_lu += time.time() - time_lu_solve
+    print("Time taken for LU decomposition and solve = {} s".format(time_lu))
+    verify_solution(mat, x_lu, b)
+
+    # Solve using SVD
+    print("Solve using SVDs")
+    time_svd = time.time()
+    u, sigma, vh = get_svd_decomposition(mat)
+    time_svd = time.time() - time_svd
+    check_svd_correctness(mat, u, sigma, vh)
+    time_svd_solve = time.time()
+    x_svd = solve_using_svd(u, sigma, vh, b)
+    time_svd += time.time() - time_svd_solve
+    print("Time taken for SVD decomposition and solve = {} s".format(time_svd))
+    verify_solution(mat, x_svd, b)
+
+    # Solve using Cholesky
+    print("Solve using Cholesky")
+    time_cholesky = time.time()
+    L = get_cholesky_decomposition(-1.0 * mat)
+    time_cholesky = time.time() - time_cholesky
+    check_cholesky_correctness(-1.0 * mat, L)
+    time_cholesky_solve = time.time()
+    x_cholesky = solve_using_cholesky(L, -1.0 * b)
+    time_cholesky += time.time() - time_cholesky_solve
+    print("Time taken for Cholesky factorization and Solve = {} s".format(time_cholesky))
+    verify_solution(mat, x_cholesky, b)
+    
+    # Solve using np.linalg.solve
+    print("Solve using numpy.linalg.solve")
+    time_np = time.time()
+    x = solve_using_np_solve(mat, b)
+    time_np = time.time() - time_np
+    print("Time taken for Numpy factorization and Solve = {} s".format(time_np))
+    verify_solution(mat, x, b)
+
+if __name__ == '__main__':
+    m = 4000
+    run_experiment(m)
